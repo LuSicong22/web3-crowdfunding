8a61d0374b482c3ad836636d841168c5752bdbc8
simon <simon.lu@tron.network>
2023-03-10 17:44:05 +0800

    c

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -18,10 +18,10 @@ contract CrowdFunding {
 
     uint256 public numberOfCampaigns = 0;
 
-    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline,string memory _image) public returns (uint256){ 
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
         Campaign storage campaign = campaigns[numberOfCampaigns];
 
-        require(campaign.deadline<block.timestamp,"The deadline should be a date in the future.");
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
 
         campaign.owner = _owner;
         campaign.title = _title;
@@ -32,37 +32,38 @@ contract CrowdFunding {
         campaign.image = _image;
 
         numberOfCampaigns++;
-        return numberOfCampaigns-1;
+
+        return numberOfCampaigns - 1;
     }
 
-    function donateToCampaign(uint256 _id) public payable{
+    function donateToCampaign(uint256 _id) public payable {
         uint256 amount = msg.value;
 
         Campaign storage campaign = campaigns[_id];
 
         campaign.donators.push(msg.sender);
-
         campaign.donations.push(amount);
 
-        (bool sent,) = payable(campaign.owner).call{value:amount}("");
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
 
-        if(sent){
+        if(sent) {
             campaign.amountCollected = campaign.amountCollected + amount;
         }
-
     }
 
-    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory){
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
         return (campaigns[_id].donators, campaigns[_id].donations);
     }
 
-    function getCampaign() public view returns (Campaign[] memory){
+    function getCampaigns() public view returns (Campaign[] memory) {
         Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
 
-        for (uint256 i = 0; i < numberOfCampaigns; i++) {
+        for(uint i = 0; i < numberOfCampaigns; i++) {
             Campaign storage item = campaigns[i];
+
             allCampaigns[i] = item;
         }
+
         return allCampaigns;
     }
 }
